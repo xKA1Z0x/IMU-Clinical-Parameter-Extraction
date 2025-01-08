03e2df1611eb25eaa7b67aa8d109df942f32f3e7
Sajjad Daneshgar <sdaneshgar@sralab.org>
2025-01-08 11:21:03 -0600

    Add files via upload

--- Walk/OrientationCorrection.m
+++ Walk/OrientationCorrection.m
@@ -50,33 +50,33 @@ function data = OrientationCorrection(data, bodyside, columnIndices, minProminen
                     end
                 end
                 
-                % Plot the results
-                fig = figure;
-
-                subplot(4, 1, 1)
-                plot(y, "Color", "black")
-                title("Difference Array")
-
-                subplot(4, 1, 2)
-                plot(y, "Color", "black")
-                title("Packet Loss Markers")
-                hold on
-                plot(maxIndices(:, 1), y(maxIndices(:, 1)), "^", "Color", "r")
-                plot(minIndices(:, 1), y(minIndices(:, 1)), "v", "Color", "r")
-                hold off
-
-                subplot(4, 1, 3)
-                plot(numericArray, "Color", "b")
-                title("Original Array")
-
-                subplot(4, 1, 4)
-                plot(result, "Color", "b")
-                title("Final Corrected Array")
-
-                sgtitle(['Body Side: ', bodyside{bs}, ', Column ', num2str(x), ', Cell ', num2str(cellIndex)]);
-
-                % Pause execution until the figure is closed
-                uiwait(fig);
+%                 % Plot the results
+%                 fig = figure;
+% 
+%                 subplot(4, 1, 1)
+%                 plot(y, "Color", "black")
+%                 title("Difference Array")
+% 
+%                 subplot(4, 1, 2)
+%                 plot(y, "Color", "black")
+%                 title("Packet Loss Markers")
+%                 hold on
+%                 plot(maxIndices(:, 1), y(maxIndices(:, 1)), "^", "Color", "r")
+%                 plot(minIndices(:, 1), y(minIndices(:, 1)), "v", "Color", "r")
+%                 hold off
+% 
+%                 subplot(4, 1, 3)
+%                 plot(numericArray, "Color", "b")
+%                 title("Original Array")
+% 
+%                 subplot(4, 1, 4)
+%                 plot(result, "Color", "b")
+%                 title("Final Corrected Array")
+% 
+%                 sgtitle(['Body Side: ', bodyside{bs}, ', Column ', num2str(x), ', Cell ', num2str(cellIndex)]);
+% 
+%                 % Pause execution until the figure is closed
+%                 uiwait(fig);
 
                 % Update the original numeric array in the cell
                 cellArray{cellIndex} = result;
